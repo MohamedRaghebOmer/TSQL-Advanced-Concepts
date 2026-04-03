42855407b4a0aed23b59edda85c27050ea42a6f7
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-04-03 12:00:11 +0200

    Learn & Practice: Variable table and temp table

---
+++ 07_TempTable/01_VariableTable.sql
@@ -0,0 +1,32 @@
+
+DECLARE @EmployeesTable TABLE
+(
+	EmployeeId INT,
+	EmployeeName NVARCHAR(50),
+	DepartmentId INT
+);
+
+
+-- Insert values
+INSERT INTO @EmployeesTable([EmployeeId], [EmployeeName], [DepartmentId])
+VALUES (1, 'Mohamed', 1);
+
+INSERT INTO @EmployeesTable([EmployeeId], [EmployeeName], [DepartmentId])
+VALUES (2, 'Ahmed', 2);
+
+-- Read from the variable table
+SELECT * FROM @EmployeesTable;
+
+-- Read with condition
+SELECT * FROM @EmployeesTable
+WHERE [EmployeeId] = 1;
+
+-- Update statement
+UPDATE @EmployeesTable
+SET [EmployeeName] = 'Ibrahem'
+WHERE [EmployeeId] = 1;
+
+-- Delete statement
+DELETE FROM @EmployeesTable
+WHERE [EmployeeId] = 2;
+
