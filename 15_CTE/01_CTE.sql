6e0cbf3251b021b0bf4a144d3a866b4c6d49a7f7
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-04-04 11:52:36 +0200

    Learn & Practice: CTE

---
+++ 15_CTE/01_CTE.sql
@@ -0,0 +1,40 @@
+--Sub Query
+select * from 
+(
+  SELECT EmployeeId, Name, Sales
+    FROM Employees6
+    WHERE Department = 'Sales'
+) SalesStaff ;
+
+
+
+--CTE
+WITH SalesStaff AS
+(
+    SELECT EmployeeId, Name, Sales
+    FROM Employees6
+    WHERE Department = 'Sales'
+)
+SELECT * FROM SalesStaff;
+
+-------------------------------------------------------
+
+--Using Sub Query
+select * from 
+(
+
+ SELECT Department, SUM(Sales) AS TotalSales
+    FROM Employees6
+    GROUP BY Department
+
+) DepartmentSales;
+
+
+-- Using CTE
+WITH DepartmentSales AS
+(
+    SELECT Department, SUM(Sales) AS TotalSales
+    FROM Employees6
+    GROUP BY Department
+)
+SELECT Department, TotalSales FROM DepartmentSales;
